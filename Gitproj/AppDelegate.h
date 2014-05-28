1f20aba4ac207bad617120fe5a8b6f53dc6ebff0
rahul101 <rahul.16jan91@gmail.com>
2014-05-28 11:56:08 +0530

    Initial Commit

---
+++ Gitproj/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Gitproj
+//
+//  Created by Rahul Sharma on 28/05/14.
+//  Copyright (c) 2014 Rahul Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Gitproj/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Gitproj
+//
+//  Created by Rahul Sharma on 28/05/14.
+//  Copyright (c) 2014 Rahul Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
